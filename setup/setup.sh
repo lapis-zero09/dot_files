17bb2b4b9e1e9b57b73ed97245a9d7588e5ef9b2
lapis-zero09 <shandy1124gaff@gmail.com>
2018-04-29 01:54:28 +0900

    fix setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -3,9 +3,11 @@
 if ! type brew > /dev/null 2&>1; then
   if [[ `uname` == 'Darwin' ]]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    brew install curl wget git tmux zsh nvim rettach-to-user-namespace docker pyenv rbenv ruby-build #ndenv
+     #ndenv
+    # zplug
     curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
-
+    # tpm
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
     # git clone https://github.com/riywo/node-build.git $(ndenv root)/plugins/node-build
 
     echo "Write /usr/local/bin/zsh in /etc/shells"
@@ -16,9 +18,12 @@ if ! type brew > /dev/null 2&>1; then
   fi
 fi
 
-if ! type brew > /dev/null 2&>1; then
-
-
 if ! type apm > /dev/null 2&>1; then
+    brew install curl wget git tmux zsh nvim docker pyenv rbenv ruby-build powerline ghq
     brew cask install atom slack notion spotify mendeley firefox google-chrome docker basictex
-end
+    brew tap sanemat/font
+    brew install ricty --patch-in-place --powerline --vim-powerline
+    cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
+    fc-cache -vf
+    git config --global ghq.root ~/Documents
+fi
