fdf4be01b0b9868c291eedc4e25557e8d1acfb9b
lapis-zero09 <lapis-zero09@klis.tsukuba.ac.jp>
2018-05-06 19:53:17 +0900

    add ndenv in setup & fix setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -3,12 +3,10 @@
 if ! type brew > /dev/null 2&>1; then
   if [[ `uname` == 'Darwin' ]]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-     #ndenv
     # zplug
     curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
     # tpm
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-    # git clone https://github.com/riywo/node-build.git $(ndenv root)/plugins/node-build
 
     echo "Write /usr/local/bin/zsh in /etc/shells"
     echo "chsh -s /usr/local/bin/zsh"
@@ -20,7 +18,7 @@ fi
 
 if ! type apm > /dev/null 2&>1; then
     brew install curl wget git tmux zsh nvim docker pyenv rbenv ruby-build powerline ghq
-    brew cask install atom slack notion spotify mendeley firefox google-chrome docker
+    brew cask install atom slack notion spotify mendeley firefox google-chrome docker postman burp-suite visual-studio-code
     brew tap sanemat/font
     brew install ricty --patch-in-place --powerline --vim-powerline
     cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
@@ -28,17 +26,21 @@ if ! type apm > /dev/null 2&>1; then
     git config --global ghq.root ~/Documents
 fi
 
-
-
+if ! type ndenv > /dev/null 2&>1; then
+    # ndenv
+    brew install ndenv
+    git clone https://github.com/riywo/node-build.git $(ndenv root)/plugins/node-build
+fi
 # tex configuration
-brew cask install basictex
-sudo tlmgr update --self --all
-sudo tlmgr install uplatex latexmk collection-langjapanese
-sudo tlmgr install algorithmicx
-sudo tlmgr install algorithms
-if [[ `uname` == 'Darwin' ]]; then
-    cd /usr/local/texlive/2017basic/texmf-dist/scripts/cjk-gs-integrate
-    sudo perl cjk-gs-integrate.pl --link-texmf --force
-    sudo mktexlsr
-    sudo kanji-config-updmap-sys hiragino-elcapitan-pron
+
+if ! type tlmgr > /dev/null 2&>1; then
+    brew cask install basictex
+    sudo tlmgr update --self --all
+    sudo tlmgr install uplatex latexmk collection-langjapanese algorithmicx algorithms
+    if [[ `uname` == 'Darwin' ]]; then
+        cd /usr/local/texlive/2017basic/texmf-dist/scripts/cjk-gs-integrate
+        sudo perl cjk-gs-integrate.pl --link-texmf --force
+        sudo mktexlsr
+        sudo kanji-config-updmap-sys hiragino-elcapitan-pron
+    fi
 fi
