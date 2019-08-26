88929218e29acd129677d440ebd13c4ae5560b00
r_kono_shinsaku01 <shinsaku_kono@r.recruit.co.jp>
2019-08-26 18:01:01 +0900

    update

--- backup.sh
+++ backup.sh
@@ -5,9 +5,7 @@ cp ~/.tmux.conf tmux.conf
 cp -r ~/.config/dein .
 cp -r ~/.config/nvim .
 cp -r ~/.config/brewfile .
-
 cp ~/.config/gitignore_global .
 cp ~/.config/gitcommit_templete .
 cp ~/.config/zsh/*.zsh zsh/
 cp ~/.latexmkrc latexmkrc
-cp -r ~/.config/brewfile .
